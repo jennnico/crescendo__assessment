2c0dfd135647c7826aa89e6ae4e8a6e61d1c3823
Jennifer <32855060+jennnico@users.noreply.github.com>
2022-01-04 20:36:26 -0500

    Update scripts.js
    Desktop complete. Mobile carousel started.

--- scripts.js
+++ scripts.js
@@ -1,54 +1,96 @@
 $(document).ready(function() {
 
+  //add sticky nav when you scroll down
   $(window).scroll(function(){
 
-    /*Sticky nav*/
     var $this = $(this);
-    var headerHeight = $('.panel--heading').outerHeight();
+    var headerHeight = $('.panel--heading').outerHeight(); //find the height
 
-    if($this.scrollTop() > headerHeight - 200){
+    if($this.scrollTop() > headerHeight - 200){ //calculate the scroll
         nav.addClass("sticky");
     } else{
         nav.removeClass("sticky");
-    } /*sticky-nav*/
+    }
 
-  }); /*window scroll*/
+  });
 
 
-//Hamburger
+//mobile hamburger
 var nav = $('.navigation');
 var stickyNav = $('.sticky');
-var mobileNav = $('.navigation__items');
-// var home = $('#home');//main header text
+var navItems = $('.navigation__items');
+var burger = $('.navigation__burger');
 
-$('.navigation__burger').click(function(){
 
-  //toggle mobile nav and rhombuses
-  if((mobileNav).hasClass('mobile-nav')){
+burger.click(function(){
 
-    mobileNav.removeClass('mobile-nav').animate({right: "-100%"});
-    $('body').css("overflow", "auto");
+  if((navItems).hasClass('mobile-nav')){ //if the menu is alredy open
 
-    setTimeout(function () {
-         $('.navigation.sticky').removeClass('sticky--show');
-    }, 300);
+    $(this).removeClass('close'); //toggles red X and burger
+    navItems.animate({right: "-100%"}); //move navigation over where screen readers can still access it
+    setTimeout(function () { //hide nav items BEFORE adjusting styles below
+         nav.css("overflow", "hidden"); //prevents horizontal scroll to hidden nav items
+         $('body').css("overflow", "visible");
+         navItems.removeClass('mobile-nav')
+    }, 400);
 
-  }else{
+  } else{ //if the menu is closed
 
-      $('.navigation.sticky').addClass('sticky--show');
-      mobileNav.addClass('mobile-nav').animate({right: "0"});
-      $('body').css("overflow", "hidden");
+    $(this).addClass('close');
+    navItems.addClass('mobile-nav').animate({right: "0"}); //add styles before showing nav items
+    nav.css("overflow", "visible");
+    $('body').css("overflow", "hidden"); //prevents vertical scroll on mobile nav
 
-      //Close nav when the user makes a selection
-      $('a').click(function(){
-        $('.navigation.sticky').removeClass('sticky--show');
-        mobileNav.removeClass('mobile-nav').animate({right: "-100%"});
-        $('body').css("overflow", "auto");
-      });//a element click
+  }
 
-  }//else
+}); //burger
 
-}); //Hamburger
+//Close nav when the user makes a selection. similar function to clicking the red X.
+$('.navigation__items a').click(function(){
+    burger.removeClass('close');
+    navItems.removeClass('mobile-nav'); //no animation needed
+    nav.css("overflow", "hidden");
+    $('body').css("overflow", "visible");
+});
+
+
+//carousel attempt
+//https://codepen.io/flowrome/pen/abowVRE
+var slides = $('.insights__list');
+var slide = $('.insight');
+var bullet = $('.bullet');
+
+bullet.click(function(){
+    console.log($(this).attr('id'));
+    var id = $(this).attr('id');
+
+    slide.removeClass('test');
+    $(`.insight.${id}`).addClass('test');
+
+});
+
+
+
+//https://codepen.io/Jake_Woods/pen/vYYoYxo
+// buttonsWrapper.addEventListener("click", e => {
+//   if (e.target.nodeName === "BUTTON") {
+//     Array.from(buttonsWrapper.children).forEach(item =>
+//       item.classList.remove("active")
+//     );
+//     if (e.target.classList.contains("first")) {
+//       slides.style.transform = "translateX(-0%)";
+//       e.target.classList.add("active");
+//     } else if (e.target.classList.contains("second")) {
+//       slides.style.transform = "translateX(-33.33333333333333%)";
+//       e.target.classList.add("active");
+//     } else if (e.target.classList.contains('third')){
+//       slides.style.transform = 'translatex(-66.6666666667%)';
+//       e.target.classList.add('active');
+//     }
+//   }
+// });
+//first carousel attempt
+
 
 
 })
